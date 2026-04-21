b77f1d363b918c4ceb24ed99c63439685db2d130
abhishekkumar444 <abhishekatter3@gmail.com>
2026-04-21 21:42:08 +0530

    feat: initialize axios client with base URL and JWT request interceptor

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = "https://social-feed-app-backend.onrender.com/api" || 'http://localhost:5000/api';
+const isProd = true;
+
+const API_BASE_URL = isProd ? "https://social-feed-app-backend.onrender.com/api" : 'http://localhost:5000/api';
 
 const client = axios.create({
   baseURL: API_BASE_URL,
